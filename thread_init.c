092d90de6f7a744c2c32c19c2444b8fce6958d86
Shetima kyari Ali <shettimaali58@yahoo.com>
2023-01-17 21:34:36 +0100

    fixed data race in check for death

--- thread_init.c
+++ thread_init.c
@@ -6,11 +6,17 @@
 /*   By: sheali <sheali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/08 02:52:01 by sheali            #+#    #+#             */
-/*   Updated: 2023/01/16 10:15:29 by sheali           ###   ########.fr       */
+/*   Updated: 2023/01/17 18:36:13 by sheali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+// this program creates one fork for each philosopher by initializing the mutex
+// for each fork. The first fork is initialized with the last fork, meaning that
+// the first philosopher will use the last fork as its right fork, and the last
+// and the rest
+// of the forks are initialized with the previous fork.
 
 // ft_get_time is a function that returns the current time in milliseconds 
 // using the gettimeofday function, which is a function that returns the current
@@ -35,17 +41,26 @@ long int	ft_get_time(void)
 // the thread to exit.
 void	*check_chow_xn(void *data)
 {
+	int		exit;
 	t_philo	*philo;
 
 	philo = (t_philo *) data;
 	pthread_mutex_lock(&philo->data->write);
 	printf("data val: %d", philo->data->dead);
 	pthread_mutex_unlock(&philo->data->write);
-	while (philo->data->dead == 0)
+	exit = 0;
+	while (!exit)
 	{
 		pthread_mutex_lock(&philo->lock);
+		pthread_mutex_lock(&philo->data->lock);
 		if (philo->data->finished >= philo->data->philo_n)
+		{
+			pthread_mutex_lock(&philo->data->write);
 			philo->data->dead = 1;
+			pthread_mutex_unlock(&philo->data->write);
+			exit = 1;
+		}
+		pthread_mutex_unlock(&philo->data->lock);
 		pthread_mutex_unlock(&philo->lock);
 	}
 	return ((void *)0);
@@ -60,10 +75,12 @@ void	*check_chow_xn(void *data)
 // finished variable by 1 to indicate that a philosopher has finished eating.
 void	*death_checker(void *philo_ptr)
 {
+	int		exit;
 	t_philo	*philo;
 
 	philo = (t_philo *) philo_ptr;
-	while (philo->data->dead == 0)
+	exit = 0;
+	while (!exit)
 	{
 		pthread_mutex_lock(&philo->lock);
 		if (ft_get_time() >= philo->time_to_die && philo->eating == 0)
@@ -75,6 +92,9 @@ void	*death_checker(void *philo_ptr)
 			philo->eat_count++;
 			pthread_mutex_unlock(&philo->data->lock);
 		}
+		pthread_mutex_lock(&philo->data->write);
+		exit = philo->data->dead;
+		pthread_mutex_unlock(&philo->data->write);
 		pthread_mutex_unlock(&philo->lock);
 	}
 	return ((void *)0);
@@ -90,16 +110,23 @@ void	*death_checker(void *philo_ptr)
 // to exit.
 void	*exec_action(void *philo_ptr)
 {
+	int		exit;
 	t_philo	*philo;
 
 	philo = (t_philo *) philo_ptr;
 	philo->time_to_die = philo->data->time_to_die + ft_get_time();
 	if (pthread_create(&philo->thread, NULL, &death_checker, (void *)philo))
 		return ((void *)1);
-	while (philo->data->dead == 0)
+	exit = 0;
+	while (!exit)
 	{
 		ft_chow(philo);
 		print_message(THINKING, philo);
+		pthread_mutex_lock(&philo->lock);
+		pthread_mutex_lock(&philo->data->write);
+		exit = philo->data->dead;
+		pthread_mutex_unlock(&philo->data->write);
+		pthread_mutex_unlock(&philo->lock);
 	}
 	if (pthread_join(philo->thread, NULL))
 		return ((void *)1);
